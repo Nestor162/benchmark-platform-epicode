77df7b71023c8b67d58427a5ec732452bcaf7312
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-02-20 13:09:44 +0100

    js-first-step

---
+++ js/script.js
@@ -0,0 +1,33 @@
+class Quiz {
+    constructor(_category, _type, _difficulty, _question, _correct, _incorrect, _time) {
+
+        this.category = _category;
+        this.type = _type;
+        this.difficulty = _difficulty;
+        this.question = _question;
+        this.correct = _correct;
+        this.incorrect = _incorrect;
+        this.time = _time;
+    }
+}
+
+let quiz1 = new Quiz("Science: Computers", "multiple", "easy", "What does CPU stand for?", "Central Processing Unit", ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"]);
+
+let quiz2 = new Quiz("Science: Computers", "multiple", "easy", "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?", "Final", ["Static", "Private", "Public"]);
+
+let quiz3 = new Quiz("Science: Computers", "boolean", "easy", "The logo for Snapchat is a Bell.", "False", ["True"]);
+
+let quiz4 = new Quiz("Science: Computers", "boolean", "easy", "Pointers were not used in the original C programming language; they were added later on in C++.", "False", ["True"]);
+
+let quiz5 = new Quiz("Science: Computers", "multiple", "easy", "What is the most preferred image format used for logos in the Wikimedia database?", ".svg", [".png", ".jpeg", ".gif"]);
+
+let quiz6 = new Quiz("Science: Computers", "multiple", "easy", "In web design, what does CSS stand for?", "Cascading Style Sheet", ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"]);
+
+let quiz7 = new Quiz("Science: Computers", "multiple", "easy", "What is the code name for the mobile operating system Android 7.0?", "Nougat", ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow",],);
+
+let quiz8 = new Quiz("Science: Computers", "multiple", "easy", "On Twitter, what is the character limit for a Tweet?", "140", ["120", "160", "100"]);
+
+let quiz9 = new Quiz("Science: Computers", "boolean", "easy", "Linux was first created as an alternative to Windows XP.", "False", ["True"]);
+
+
+let quiz10 = new Quiz("Science: Computers", "multiple", "easy", "Which programming language shares its name with an island in Indonesia?", "Java", ["Python", "C", "Jakarta"]);
