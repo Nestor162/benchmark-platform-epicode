ac6b5e89e2ea55cf3047df63380cd6ac3a8f2c7e
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-02-20 14:03:15 +0100

    logo e sfondo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//IMG//
+
+let img = document.createElement('img');
+img.src = 'assets/img/epicode_logo.png';
+let tag = document.querySelector("body");
+img.className += 'logo'
+tag.append(img);
+
+//
