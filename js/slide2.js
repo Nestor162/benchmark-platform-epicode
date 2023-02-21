c05cb9bcbb070418169575bcb7843cc585a66c09
Salvo1789 <salvo.mercurio89@gmail.com>
2023-02-21 14:26:24 +0100

    version 0.4

---
+++ js/slide2.js
@@ -0,0 +1,172 @@
+
+//////////QUIZ/////////////////
+
+class Quiz {
+    constructor(
+        _category,
+        _type,
+        _difficulty,
+        _question,
+        _correct,
+        _incorrect,
+        _time
+    ) {
+        this.category = _category;
+        this.type = _type;
+        this.difficulty = _difficulty;
+        this.question = _question;
+        this.correct = _correct;
+        this.incorrect = _incorrect;
+        this.time = _time;
+    }
+}
+
+let quiz1 = new Quiz(
+    "Science: Computers",
+    "multiple",
+    "easy",
+    "What does CPU stand for?",
+    "Central Processing Unit",
+    [
+        "Central Process Unit",
+        "Computer Personal Unit",
+        "Central Processor Unit",
+    ],
+    20
+);
+
+let quiz2 = new Quiz(
+    "Science: Computers",
+    "multiple",
+    "easy",
+    "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+    "Final",
+    ["Static", "Private", "Public"],
+    10
+);
+
+let quiz3 = new Quiz(
+    "Science: Computers",
+    "boolean",
+    "easy",
+    "The logo for Snapchat is a Bell.",
+    "False",
+    ["True"],
+    10
+);
+
+let quiz4 = new Quiz(
+    "Science: Computers",
+    "boolean",
+    "easy",
+    "Pointers were not used in the original C programming language; they were added later on in C++.",
+    "False",
+    ["True"],
+    15
+);
+
+let quiz5 = new Quiz(
+    "Science: Computers",
+    "multiple",
+    "easy",
+    "What is the most preferred image format used for logos in the Wikimedia database?",
+    ".svg",
+    [".png", ".jpeg", ".gif"],
+    25
+);
+
+let quiz6 = new Quiz(
+    "Science: Computers",
+    "multiple",
+    "easy",
+    "In web design, what does CSS stand for?",
+    "Cascading Style Sheet",
+    [
+        "Counter Strike: Source",
+        "Corrective Style Sheet",
+        "Computer Style Sheet",
+    ],
+    20
+);
+
+let quiz7 = new Quiz(
+    "Science: Computers",
+    "multiple",
+    "easy",
+    "What is the code name for the mobile operating system Android 7.0?",
+    "Nougat",
+    ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    15
+);
+
+let quiz8 = new Quiz(
+    "Science: Computers",
+    "multiple",
+    "easy",
+    "On Twitter, what is the character limit for a Tweet?",
+    "140",
+    ["120", "160", "100"],
+    24
+);
+
+let quiz9 = new Quiz(
+    "Science: Computers",
+    "boolean",
+    "easy",
+    "Linux was first created as an alternative to Windows XP.",
+    "False",
+    ["True"],
+    18
+);
+
+let quiz10 = new Quiz(
+    "Science: Computers",
+    "multiple",
+    "easy",
+    "Which programming language shares its name with an island in Indonesia?",
+    "Java",
+    ["Python", "C", "Jakarta"],
+    30
+);
+
+let quizQuest = [
+    quiz1,
+    quiz2,
+    quiz3,
+    quiz4,
+    quiz5,
+    quiz6,
+    quiz7,
+    quiz8,
+    quiz9,
+    quiz10,
+];
+
+/*function getRandomQuiz(){
+    let output = [];
+    quizQuest.forEach((question, questionNumber) => {
+        let answers = [];
+        for (letter in question.)
+    }
+)}*/
+
+function getRandomQuiz(n) {
+    let output = [];
+    let usedQuestions = []; // array di domande già usate
+
+    // ciclo finché non si sono aggiunte tutte le domande richieste
+    while (output.length < n) {
+        // scegli un indice casuale
+        let randomIndex = Math.floor(Math.random() * quizQuest.length);
+
+        // se la domanda non è già stata usata, la aggiungiamo all'output
+        if (!usedQuestions.includes(randomIndex)) {
+            usedQuestions.push(randomIndex);
+            output.push(quizQuest[randomIndex]);
+        }
+    }
+
+    return output;
+}
+
+console.log(getRandomQuiz(5));
