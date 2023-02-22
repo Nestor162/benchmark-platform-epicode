9945d96a02349741ceec64157522f8ec59eff5b2
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-02-22 15:12:46 +0100

    stelline

--- js/valutazione.js
+++ js/valutazione.js
@@ -6,11 +6,36 @@ const starUl = createElements(main,'ul','main')
 
 for(let x=0;x<10;x++){
     const star = createElements(starUl,'li','star');
-    const img = createElements(star,'img')
-    img.src = 'assets/img/star.svg';
+
+    //qui dovrebbe andarci l'svg
+    star.innerHTML = '&#10029'
+    star.starValue = (x+1);
+    ['mouseover', 'mouseout', 'click'].forEach((ele)=>{
+        star.addEventListener(ele,starRate);
+    })
 } 
 
+function starRate(e){
+    const eventType = e.type
+    const parent = e.target.closest('.stars');
+    const colorStars = parent.querySelectorAll('.star');
+    if(eventType === 'mouseover'){
+        colorRate(colorStars);
+    }
 
+    colorRate(colorStars,e.target.starValue);
+}
+
+
+function colorRate(colorStars,val){
+    colorStars.forEach((star,index)=>{
+        if(index < val){
+            star.classList.add('starColorHover');
+        }else{
+            star.classList.remove('starColorHover');
+        }
+    })
+} 
 
 //funzione per creare elementi
 
