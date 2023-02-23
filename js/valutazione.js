c256cacb0186bd7d6a74e05c59f1f20cd2302a8e
maryemonty <113999089+maryemonty@users.noreply.github.com>
2023-02-23 10:57:41 +0100

    pagina feedback

--- js/valutazione.js
+++ js/valutazione.js
@@ -1,12 +1,25 @@
-const main = document.querySelector('.stars')
+const main = document.createElement('div')
+main.className += 'stars'
+const tag = document.querySelector("body");
 
+
+//titolo 'tell us how it's going'
+
+const h1 = createElements(tag, 'h1', 'h1')
+h1.innerHTML = 'Tell us how it\'s going'
+
+
+//paragrafo
+
+const p = createElements(tag,'h3','p')
+p.innerHTML = 'From 0 to 10, how likely are you to recommend EPICODE to a friend o a colleague?'
 
 //ciclo stelline
-
+tag.append(main)
 
 for(let x=0;x<10;x++){
-    const star = createElements(main,'object','star')
-    star.data = '../assets/img/star.svg'
+    const star = createElements(main,'img','star')
+    star.src = '../assets/img/star.svg'
     star.starValue = (x+1);
     ['mouseover', 'mouseout', 'click'].forEach((ele)=>{
         star.addEventListener(ele,starRate);
@@ -34,6 +47,26 @@ function colorRate(colorStars,val){
         }
     })
 } 
+
+//invito a lasciare una valutazione
+
+const pValutazione = createElements(tag,'h3','pValutazione')
+pValutazione.innerHTML = 'Leave us an open feedback about your experience so far'
+
+//Input
+const inputText = document.createElement('div')
+inputText.className += 'inputText'
+tag.append(inputText)
+
+const input = createElements(inputText,'input','inp')
+input.type = 'text'
+input.placeholder = 'Write your comment here'
+
+//bottone sotto
+
+const button = createElements(tag,'button','button');
+button.innerHTML = "more info".toUpperCase();
+button.id += 'moreInfoPosition'
 
 //funzione per creare elementi
 
